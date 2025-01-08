04271d88c56fda85fa55a29d94507261a51a498d
ekadhirapratama <eka.dhirapratama@gmail.com>
2025-01-08 22:57:58 +0700

    feat: add swagger

--- src/app.js
+++ src/app.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const apiRoutes = require('./routes/apiV1Routes');
+const path = require('path');
 
 const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Favicon
+app.get('/favicon.ico', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/favicon.ico'));
+});
+
+// Route untuk file Swagger JSON
+app.get('/swagger.json', (req, res) => {
+  res.sendFile(path.join(__dirname, '../swagger.json'));
+});
+
+// Route utama untuk dokumentasi API
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/docs.html'));
+});
 
 // Routes
 app.use('/api/v1', apiRoutes);
